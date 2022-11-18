92f80232b3f93802fa6ae00b7cdf157b93dcfb3c
Christian Liebel <christian.liebel@thinktecture.com>
2022-11-18 09:25:41 +0100

    Add TodosService

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,36 +1,46 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Todo } from 'src/todo/todo';
 
 @Controller('todos')
 export class TodosController {
-    @Get()
-    getAllTodos(): Todo[] {
-        return [{
-            id: 1,
-            name: "Wäsche waschen",
-            done: false
-        }];
-    }
+  @Get()
+  getAllTodos(): Todo[] {
+    return [
+      {
+        id: 1,
+        name: 'Wäsche waschen',
+        done: false,
+      },
+    ];
+  }
 
-    @Get(':id')
-    getSingleTodo(@Param('id') id: string): Todo {
-        return this.getAllTodos()[0];
-        // throw new NotFoundError();
-    }
+  @Get(':id')
+  getSingleTodo(@Param('id') id: string): Todo {
+    return this.getAllTodos()[0];
+    // throw new NotFoundError();
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    createNewTodo(@Body() body: Todo): Todo {
-        return { ...body, id: 2 };
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createNewTodo(@Body() body: Todo): Todo {
+    return { ...body, id: 2 };
+  }
 
-    @Put(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    updateTodo(@Param('id') id: string, @Body() body: Todo): void {
-    }
+  @Put(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  updateTodo(@Param('id') id: string, @Body() body: Todo): void {}
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    deleteTodo(@Param('id') id: string): void {
-    }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteTodo(@Param('id') id: string): void {}
 }
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { Todo } from "../todo/todo";
+
+@Injectable()
+export class TodosService {
+  private readonly store = new Map<number, Todo>();
+  private nextId = 1;
+
+  getAll(): Todo[] {
+    return Array.from(this.store.values());
+  }
+
+  get(id: number): Todo | undefined {
+    return this.store.get(id);
+  }
+
+  add(name: string): Todo {
+    const todo: Todo = { name, done: false, id: this.nextId++ };
+    this.store.set(todo.id, todo);
+    return todo;
+  }
+
+  update(id: number, todo: Todo): void {
+    this.store.set(id, { id, name: todo.name, done: todo.done });
+  }
+
+  delete(id: number): void {
+    this.store.delete(id);
+  }
+}
