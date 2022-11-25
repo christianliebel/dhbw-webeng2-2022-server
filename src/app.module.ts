2ef386c22357b09aee12f22dc34e50b1b82e19c2
Christian Liebel <christian.liebel@thinktecture.com>
2022-11-25 09:17:07 +0100

    Wire middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
+import { AuthenticationMiddleware } from './common/authentication.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, TodosController],
   providers: [AppService, TodosService],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthenticationMiddleware).forRoutes(TodosController);
+  }
+}
